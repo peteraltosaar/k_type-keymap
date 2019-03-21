2680df26496c9945b8bac7ea2c92dd761a000bcb
Peter Altosaar <peter.altosaar@gmail.com>
2019-03-21 11:30:17 -0400

    Add Todoist, Character Viewer shortcuts, and PW shortcuts

--- keymap.c
+++ keymap.c
@@ -22,6 +22,8 @@
 #define ALTRGHT LALT(KC_RGHT)
 #define GUILEFT LGUI(KC_LEFT)
 #define GUIRGHT LGUI(KC_RGHT)
+#define CHARS LGUI(LCTL(KC_SPC))
+#define ADDTODO LGUI(LSFT(KC_A))
 
 // IntelliJ Shortcuts
 #define FNDFILE LSFT(LCTL(KC_N))
@@ -74,7 +76,7 @@ enum custom_keycodes {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QW] = KEYMAP(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           LOCKSCR, KC_SLCK, KC_PAUS, \
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           ADDTODO,   CHARS, LOCKSCR, \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, \
       ARR_ESC, KC_A,    KC_S,    KC_D,    INTJ_F,    KC_G,    KC_H,    INTJ_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
