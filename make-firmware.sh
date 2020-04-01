a92fa1589e1f726367eb8b2827439d68cb5617e7
Peter Altosaar <peter.alrtosaar@gmail.com>
2020-04-01 10:46:10 -0400

    Add dynamic password input

--- make-firmware.sh
+++ make-firmware.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Password:"
+read -s password
+
 cp keymap.c ../qmk_firmware/keyboards/k_type/keymaps/peteraltosaar/
 cd ../qmk_firmware
+sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/k_type/keymaps/peteraltosaar/keymap.c
 make k_type:peteraltosaar
+rm keyboards/planck/keymaps/peteraltosaar/keymap.c
 cd -
 cp ../qmk_firmware/k_type_peteraltosaar.bin .
