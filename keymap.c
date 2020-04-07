5026ae0e8ab91426951230c5fb459631be1c57cf
Peter Altosaar <peter.alrtosaar@gmail.com>
2020-04-07 09:39:16 -0400

    Make use of actual QMK delay function

--- keymap.c
+++ keymap.c
@@ -84,7 +84,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           ADDTODO,   CHARS, LOCKSCR, \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN, \
-      ARR_ESC, KC_A,    KC_S,    KC_D,    INTJ_F,    KC_G,    KC_H,    INTJ_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+      ARR_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
       KC_LSFT, INTJ_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                            KC_UP, \
       KC_LCTL, KC_LALT, KC_LGUI,          SPC_APP,                    KC_RALT, KC_RGUI,   INTJ,  KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -101,7 +101,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
       _______,   CLOSE, _______,     END, REFACTR, GO2TEST, _______, _______, PREVMTD, OUTLINE, _______, _______, _______, _______, _______, _______, _______, \
       HIDWINS, RUNTGTS,  SYMBOL,   DEBUG,    FIND,     GIT, HIERARC,    BACK, NEXTMTD, FORWARD, FONTRES, _______, _______, \
-      _______,   RERUN, EXECUTE,  CREATE, EXT_VAR, _______, FNDFILE,    MENU, FONT_DN, FONT_UP,    INFO, _______,                            PRVCHNG, \
+      _______,  INTJ_Z, EXECUTE,  CREATE, EXT_VAR, _______, FNDFILE,    MENU, FONT_DN, FONT_UP,    INFO, _______,                            PRVCHNG, \
       _______, _______, _______,          _______,                   _______, _______, _______, _______,                            _______, NXTCHNG, _______),
 
     [_APPS] = KEYMAP(
@@ -123,13 +123,6 @@ void matrix_scan_user(void) {
 
 };
 
-void delay(void) {
-  int dummy = 0;
-  for (int i = 0; i < 1000; i++) {
-    dummy++;
-  }
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case PW:
@@ -150,71 +143,61 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case BRAVE:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("brave" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "brave" SS_TAP(X_ENTER));
       }
       return false;
     case FIREFOX:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("firefox" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "firefox" SS_TAP(X_ENTER));
       }
       return false;
     case ITERM:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("iterm" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "iterm" SS_TAP(X_ENTER));
       }
       return false;
     case MAIL:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("mail" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "mail" SS_TAP(X_ENTER));
       }
       return false;
     case NOTION:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("notion" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "notion" SS_TAP(X_ENTER));
       }
       return false;
     case SIGNAL:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("signal" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "signal" SS_TAP(X_ENTER));
       }
       return false;
     case SPOTIFY:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("spotify" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "spotify" SS_TAP(X_ENTER));
       }
       return false;
     case TEAMS:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("teams" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "teams" SS_TAP(X_ENTER));
       }
       return false;
     case TODOIST:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("todoist" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "todoist" SS_TAP(X_ENTER));
       }
       return false;
     case VMWARE:
       if (record->event.pressed) {
         SEND_STRING(SS_LALT(SS_TAP(X_SPACE)));
-        delay();
-        SEND_STRING("vmware" SS_TAP(X_ENTER));
+        SEND_STRING(SS_DELAY(200) "vmware" SS_TAP(X_ENTER));
       }
       return false;
   }
